24315a6d99fde313fee61268fc381d116f740497
NebbiaOne <fairhurstdylan42@gmail.com>
2018-05-18 00:03:53 +0200

    Added Folders with placeholder items

---
+++ Tribe/Tribe/Assets/Scripts/BestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Yeah look at that code, so good, so nice. You know you like it.");
+		//accurate representation of my work ethic
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
